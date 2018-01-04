43b3a4d1ce225a2762b78f664ad4ddbb7ab8ebd4
kateoneill <S00165383@mail.itsligo.ie>
2018-01-04 04:00:30 +0000

    logout works, other links dead

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AuthProvider } from '../../providers/auth/auth';
+import { User } from '../../models/User';
 
 /**
  * Generated class for the AccountPage page.
@@ -15,11 +17,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AccountPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public user: User;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private _auth: AuthProvider) {
+    this.user = new User();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AccountPage');
+  }
+  logout() {
+    this._auth.logout();
   }
 
 }
