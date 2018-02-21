877f6a1ef9ac024c95d8e148fbe175bf2899450c
kateoneill <S00165383@mail.itsligo.ie>
2018-02-21 11:20:35 +0000

    remove uid appended to username

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -14,15 +14,9 @@ export class RegisterPage {
   public user: User;
   public confirmPassword: string;
   public ngForm: FormGroup;
-  public id: AAGUID;
 
   constructor(private _fb: FormBuilder, private _auth: AuthProvider) {
     this.user = new User();
-    
-    for (var i = 0; i < 100; i++) {
-      this.id = Guid.newGuid();
-      console.log(this.id);
-    }
     this.createForm();
   }
 
@@ -54,20 +48,13 @@ export class RegisterPage {
   }
 
   register() {
-    this.user.userName = this.user.firstName + this.user.lastName + this.id;
+    this.user.userName = this.user.firstName + this.user.lastName;
     this._auth.register(this.user)
   }
 
 
 }
 
-class Guid {
-  static newGuid() {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-          var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-          return v.toString(6);
-      });
-  }
-}
+
 
 
